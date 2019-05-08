a084fa459d4e273ae11b52149f3f8be761ec23fd
James <JschwartzDev@gmail.com>
2019-05-08 13:30:56 -0400

    Delete gameTwoMain.js

--- GameTwo/gameTwoMain.js
+++
@@ -1,6 +0,0 @@
-(function(){
-  let outputField = document.getElementById("outputTextArea");
-  outputField.value = "\n\n\n\n\n\n\n---------Welcome to the game!--------\nThere are 3 classes to choose from"
-   + "\n1: Warrior\n2: Rogue\n3: Paladin";
-})();
-  
