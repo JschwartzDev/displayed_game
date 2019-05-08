22d4be1db3fae4800334b55b823044c98ce9976a
James <JschwartzDev@gmail.com>
2019-05-08 15:39:14 -0400

    Add files via upload

---
+++ GameTwo/g2combat.js
@@ -0,0 +1,78 @@
+function combat(){
+  let rand = Math.floor(Math.random() * 10);
+  let enemy = enemyList[rand];
+
+  function leaveTown(){
+    let output = document.getElementById("outputTextArea");
+    output.value = "\n\n\n\n\n\n\n\n\n\n\n\n*Wandering through the environment*" +
+    "\nLook out! A " + enemy.name + " has appeared!" +
+    "\nPress enter to continue";
+
+    let inputSubmit = document.getElementById("inputSubmit");
+
+    inputSubmit.onclick = function choices(){
+
+      if(enemy.health >= 0 && player.health >= 0){
+        output.value = "\n\n\n\n\n\n\n\n\nPlayer health: " + player.health +
+        "\nEnemy health: " + enemy.health +
+        "\nType 'attack', 'heal' or 'run'" +
+        "\n1: attack" +
+        "\n2: potion + " + player.potionCount +
+        "\n3: run";
+
+        inputSubmit.onclick = function choose(){
+
+          let input = document.getElementById("inputTextArea");
+
+          let valid = {
+            "attack":"yes",
+            "Attack":"yes",
+            "potion":"yes",
+            "Potion":"yes",
+            "run":"yes",
+            "Run":"yes"
+          }
+
+          if(input.value in valid){
+            switch(input.value){
+              case "attack":
+              case "Attack":
+                let playerDmg = player.combat();
+                let enemyDmg = (enemy.attack - player.defense);
+                player.health -= enemyDmg;
+                enemy.health -= playerDmg;
+                choices();
+                break;
+              case "potion":
+              case "Potion":
+                if(player.potionCount >= 1){
+                  let input = document.getElementById("inputTextArea");
+                  input.value = "";
+                  player.health += player.potHealAmount;
+                  player.potionCount--;
+                  choices();
+                }
+                break;
+              case "run":
+              case "Run":
+                let input = document.getElementById("inputTextArea");
+                input.value = "";
+                town();
+              break;
+            }
+          }
+
+        }//end of choose()
+
+
+      } else {
+        
+        loot();
+        combat();
+      }//end of enemy health check if
+    }//end of choices()
+  } //end of leavetown()
+
+
+  leaveTown();
+}
---
+++ GameTwo/town.js
@@ -0,0 +1,50 @@
+
+function town(){
+  let output = document.getElementById("outputTextArea");
+
+  output.value = "\n\n\n\n\n\n\n\n\n\nWelcome to the world, " + player.name +
+  "\nAdventure Options - " +
+  "\n1: Combat!" +
+  "\n2: Shop" +
+  "\n3: Craft";
+
+  let inputSubmit = document.getElementById("inputSubmit");
+
+  inputSubmit.onclick = function choose(){
+
+    let input = document.getElementById("inputTextArea");
+
+    let valid = {
+      "combat": "yes",
+      "Combat": "yes",
+      "shop": "yes",
+      "Shop": "yes",
+      "Craft": "yes",
+      "craft": "yes"
+    }
+
+    if(input.value in valid){
+      switch(input.value){
+        case "combat":
+        case "Combat":
+          input.value = "";
+          combat();
+        break;
+        case "shop":
+        case "Shop":
+          input.value = "";
+          shop();
+        break;
+        case "craft":
+        case "Craft":
+          input.value = "";
+          craft();
+        break;
+      }
+    } else {
+      output.value = "Invalid input";
+      town();
+    }
+  }
+
+}
