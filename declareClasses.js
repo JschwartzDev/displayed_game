8289df0c656ddf7d3f20b41f5acf0e8c83430a88
James Schwartz <JschwartzDev@gmail.com>
2019-12-07 11:13:52 -0500

    moved out of excess folder

---
+++ declareClasses.js
@@ -0,0 +1,324 @@
+let warriorCreator = new Function("empty",`
+  this.name = "Warrior";
+  this.rand = Math.floor(Math.random() * 10);
+  this.level = 1;
+  this.exp = 0;
+  this.health = 300;
+  this.maxHealth = 300;
+  this.mana = 0;
+  this.maxMana = 1;
+  this.manaPotionCount = 0;
+  this.manaPotionRecoverAmount = this.maxMana/2;
+  this.potHealAmount = this.maxHealth/2;
+  this.potionCount = 5;
+  this.gold = 200;
+  this.strength = 12 + this.level;
+  this.intellect = 10;
+  this.agility = 10;
+  this.defense = 0;
+  this.inventory = [];
+  this.ironOreCount = 0;
+  this.ironBarCount = 0;
+  this.mithrilOreCount = 0;
+  this.mithrilBarCount = 0;
+  this.thoriumOreCount = 0;
+  this.thoriumBarCount = 0;
+  this.craftingLevel = 0;
+  this.craftingExperience = 0;
+  this.combat = function(){
+
+    let attacks = {
+      overPower: Math.floor(Math.random() * this.strength) + 5,
+      cleave: Math.floor(Math.random() * this.strength) + 5,
+      slam: Math.floor(Math.random() * this.strength) + 5,
+      maul: Math.floor(Math.random() * this.strength) + 5,
+      execute: Math.floor(Math.random() * this.strength) + 5,
+      revenge: Math.floor(Math.random() * this.strength) + 5,
+      heroicStrike: Math.floor(Math.random() * this.strength) + 5,
+      throwWeapon: Math.floor(Math.random() * this.strength) + 5,
+      hamstring: Math.floor(Math.random() * this.strength) + 5,
+      blaseFlurry: Math.floor(Math.random() * this.strength) + 5,
+    }
+
+    let keys = [];
+    function keyReader(object, array){
+      for(let key in object){
+        array.push(key);
+      }
+    }
+
+    keyReader(attacks,keys);
+    return attacks[keys[this.rand]];
+  }
+`);
+
+
+let paladinCreator = new Function("empty",`
+  this.name = "Paladin";
+  this.rand = Math.floor(Math.random() * 10);
+  this.level = 1;
+  this.exp = 0;
+  this.health = 250 * this.level;
+  this.maxHealth = 250;
+  this.mana = 0;
+  this.maxMana = 1;
+  this.manaPotionCount = 0;
+  this.manaPotionRecoverAmount = this.maxMana/2;
+  this.potHealAmount = this.maxHealth/2;
+  this.potionCount = 5;
+  this.gold = 200;
+  this.strength = 12 + this.level;
+  this.intellect = 10;
+  this.agility = 10;
+  this.defense = 0;
+  this.inventory = [];
+  this.ironOreCount = 0;
+  this.ironBarCount = 0;
+  this.mithrilOreCount = 0;
+  this.mithrilBarCount = 0;
+  this.thoriumOreCount = 0;
+  this.thoriumBarCount = 0;
+  this.craftingLevel = 0;
+  this.craftingExperience = 0;
+  this.combat = function(){
+
+    let attacks = {
+      divineStorm: Math.floor(Math.random() * this.strength) + 5,
+      judgement: Math.floor(Math.random() * this.strength) + 5,
+      holyShock: Math.floor(Math.random() * this.strength) + 5,
+      hammerOfJustice: Math.floor(Math.random() * this.strength) + 5,
+      consecration: Math.floor(Math.random() * this.strength) + 5,
+      lightOfDawn: Math.floor(Math.random() * this.strength) + 5,
+      beaconOfLight: Math.floor(Math.random() * this.strength) + 5,
+      templarsVerdict: Math.floor(Math.random() * this.strength) + 5,
+      crusaderStrike: Math.floor(Math.random() * this.strength) + 5,
+      bladeOfJustice: Math.floor(Math.random() * this.strength) + 5,
+    }
+
+    let keys = [];
+    function keyReader(object, array){
+      for(let key in object){
+        array.push(key);
+      }
+    }
+
+    keyReader(attacks,keys);
+    return attacks[keys[this.rand]];
+  }
+`);
+
+let rogueCreator = new Function("empty",`
+  this.name = "Rogue";
+  this.rand = Math.floor(Math.random() * 10);
+  this.level = 1;
+  this.exp = 0;
+  this.health = 250 * this.level;
+  this.maxHealth = 250;
+  this.mana = 0;
+  this.maxMana = 1;
+  this.manaPotionCount = 0;
+  this.manaPotionRecoverAmount = this.maxMana/2;
+  this.potHealAmount = this.maxHealth/2;
+  this.potionCount = 5;
+  this.gold = 200;
+  this.agility = 12 + this.level;
+  this.strength = 10;
+  this.intellect = 10;
+  this.defense = 0;
+  this.inventory = [];
+  this.ironOreCount = 0;
+  this.ironBarCount = 0;
+  this.mithrilOreCount = 0;
+  this.mithrilBarCount = 0;
+  this.thoriumOreCount = 0;
+  this.thoriumBarCount = 0;
+  this.craftingLevel = 0;
+  this.craftingExperience = 0;
+  this.combat = function(){
+
+    let attacks = {
+      envenom: Math.floor(Math.random() * this.agility) + 5,
+      sinisterStrike: Math.floor(Math.random() * this.agility) + 5,
+      rupture: Math.floor(Math.random() * this.agility) + 5,
+      eviscerate: Math.floor(Math.random() * this.agility) + 5,
+      backstab: Math.floor(Math.random() * this.agility) + 5,
+      ambush: Math.floor(Math.random() * this.agility) + 5,
+      shadowStep: Math.floor(Math.random() * this.agility) + 5,
+      throwWeapon: Math.floor(Math.random() * this.agility) + 5,
+      pistolShot: Math.floor(Math.random() * this.agility) + 5,
+      kidneyShot: Math.floor(Math.random() * this.agility) + 5,
+    }
+
+    let keys = [];
+    function keyReader(object, array){
+      for(let key in object){
+        array.push(key);
+      }
+    }
+
+    keyReader(attacks,keys);
+    return attacks[keys[this.rand]];
+  }
+`);
+
+let hunterCreator = new Function("empty",`
+  this.name = "Hunter";
+  this.rand = Math.floor(Math.random() * 10);
+  this.level = 1;
+  this.exp = 0;
+  this.health = 250 * this.level;
+  this.maxHealth = 250;
+  this.mana = 0;
+  this.maxMana = 1;
+  this.manaPotionCount = 0;
+  this.manaPotionRecoverAmount = this.maxMana/2;
+  this.potHealAmount = this.maxHealth/2;
+  this.potionCount = 5;
+  this.gold = 200;
+  this.intellect = 10 ;
+  this.strength = 10;
+  this.agility = 12 + this.level;
+  this.defense = 0;
+  this.inventory = [];
+  this.ironOreCount = 0;
+  this.ironBarCount = 0;
+  this.mithrilOreCount = 0;
+  this.mithrilBarCount = 0;
+  this.thoriumOreCount = 0;
+  this.thoriumBarCount = 0;
+  this.craftingLevel = 0;
+  this.craftingExperience = 0;
+  this.combat = function(){
+
+    let attacks = {
+      envenom: Math.floor(Math.random() * this.intellect) + 5,
+      sinisterStrike: Math.floor(Math.random() * this.intellect) + 5,
+      rupture: Math.floor(Math.random() * this.intellect) + 5,
+      eviscerate: Math.floor(Math.random() * this.intellect) + 5,
+      backstab: Math.floor(Math.random() * this.intellect) + 5,
+      ambush: Math.floor(Math.random() * this.intellect) + 5,
+      shadowStep: Math.floor(Math.random() * this.intellect) + 5,
+      throwWeapon: Math.floor(Math.random() * this.intellect) + 5,
+      pistolShot: Math.floor(Math.random() * this.intellect) + 5,
+      kidneyShot: Math.floor(Math.random() * this.intellect) + 5,
+    }
+
+    let keys = [];
+    function keyReader(object, array){
+      for(let key in object){
+        array.push(key);
+      }
+    }
+
+    keyReader(attacks,keys);
+    return attacks[keys[this.rand]];
+  }
+`);
+
+let shamanCreator = new Function("empty",`
+  this.name = "Shaman";
+  this.rand = Math.floor(Math.random() * 10);
+  this.level = 1;
+  this.exp = 200;
+  this.health = 250 * this.level;
+  this.maxHealth = 250;
+  this.mana = 500;
+  this.maxMana = 500;
+  this.manaPotionCount = 7;
+  this.manaPotionRecoverAmount = this.maxMana/2;
+  this.potHealAmount = this.maxHealth/2;
+  this.potionCount = 5;
+  this.gold = 200;
+  this.intellect = 12 + this.level;
+  this.strength = 10;
+  this.agility = 10;
+  this.defense = 0;
+  this.inventory = [];
+  this.ironOreCount = 0;
+  this.ironBarCount = 0;
+  this.mithrilOreCount = 0;
+  this.mithrilBarCount = 0;
+  this.thoriumOreCount = 0;
+  this.thoriumBarCount = 0;
+  this.craftingLevel = 0;
+  this.craftingExperience = 0;
+  this.combat = function(){
+
+    let attacks = {
+      envenom: Math.floor(Math.random() * this.intellect) + 5,
+      sinisterStrike: Math.floor(Math.random() * this.intellect) + 5,
+      rupture: Math.floor(Math.random() * this.intellect) + 5,
+      eviscerate: Math.floor(Math.random() * this.intellect) + 5,
+      backstab: Math.floor(Math.random() * this.intellect) + 5,
+      ambush: Math.floor(Math.random() * this.intellect) + 5,
+      shadowStep: Math.floor(Math.random() * this.intellect) + 5,
+      throwWeapon: Math.floor(Math.random() * this.intellect) + 5,
+      pistolShot: Math.floor(Math.random() * this.intellect) + 5,
+      kidneyShot: Math.floor(Math.random() * this.intellect) + 5,
+    }
+
+    let keys = [];
+    function keyReader(object, array){
+      for(let key in object){
+        array.push(key);
+      }
+    }
+
+    keyReader(attacks,keys);
+    return attacks[keys[this.rand]];
+  }
+`);
+
+let mageCreator = new Function("empty",`
+  this.name = "Mage";
+  this.rand = Math.floor(Math.random() * 10);
+  this.level = 1;
+  this.exp = 200;
+  this.health = 250 * this.level;
+  this.maxHealth = 250;
+  this.mana = 500;
+  this.maxMana = 500;
+  this.manaPotionCount = 7;
+  this.manaPotionRecoverAmount = this.maxMana/2;
+  this.potHealAmount = this.maxHealth/2;
+  this.potionCount = 3;
+  this.gold = 200;
+  this.intellect = 12 + this.level;
+  this.strength = 10;
+  this.agility = 10;
+  this.defense = 0;
+  this.inventory = [];
+  this.ironOreCount = 0;
+  this.ironBarCount = 0;
+  this.mithrilOreCount = 0;
+  this.mithrilBarCount = 0;
+  this.thoriumOreCount = 0;
+  this.thoriumBarCount = 0;
+  this.craftingLevel = 0;
+  this.craftingExperience = 0;
+  this.combat = function(){
+
+    let attacks = {
+      envenom: Math.floor(Math.random() * this.intellect) + 5,
+      sinisterStrike: Math.floor(Math.random() * this.intellect) + 5,
+      rupture: Math.floor(Math.random() * this.intellect) + 5,
+      eviscerate: Math.floor(Math.random() * this.intellect) + 5,
+      backstab: Math.floor(Math.random() * this.intellect) + 5,
+      ambush: Math.floor(Math.random() * this.intellect) + 5,
+      shadowStep: Math.floor(Math.random() * this.intellect) + 5,
+      throwWeapon: Math.floor(Math.random() * this.intellect) + 5,
+      pistolShot: Math.floor(Math.random() * this.intellect) + 5,
+      kidneyShot: Math.floor(Math.random() * this.intellect) + 5,
+    }
+
+    let keys = [];
+    function keyReader(object, array){
+      for(let key in object){
+        array.push(key);
+      }
+    }
+
+    keyReader(attacks,keys);
+    return attacks[keys[this.rand]];
+  }
+`);
---
+++ g2combat.js
@@ -0,0 +1,186 @@
+function combat(){
+  combatScene();
+
+  let leftOutPut = document.getElementById("leftOutPut");
+  let rightOutput = document.getElementById("rightOutput");
+
+
+  let rand = Math.floor(Math.random() * 10);
+  let enemy = enemyList[rand];
+  let exp = 100 + ((Math.floor(Math.random()*10) + 1) * player.level);
+  enemy.health = parseInt(player.maxHealth/2);
+
+  function leaveTown(){
+    let inputSubmit = document.getElementById("inputSubmit");
+    let input = document.getElementById("inputTextArea");
+    let output = document.getElementById("outputTextArea");
+    output.value = "\n\n\n\n\n\n\n\n\n\n\n\n*Wandering through the environment*" +
+    "\nLook out! A " + enemy.name + " has appeared!" +
+    "\nPress enter to continue";
+
+
+
+    inputSubmit.onclick = function choices(){
+      updateHealthBar();
+      if(enemy.health >= 0 && player.health >= 0){
+        output.value = "\n\n\n\n\n\n\n\nPlayer health: " + player.health +
+        "\nEnemy health: " + enemy.health +
+        "\nType 'attack', 'health', 'mana' or 'run'" +
+        "\n1: attack" +
+        "\n2: health pot + " + player.potionCount +
+        "\n2: mana pot + " + player.manaPotionCount +
+        "\n3: run";
+
+        inputSubmit.onclick = function choose(){
+          let regex = /attack|health|mana|run/i;
+          let result = regex.test(input.value);
+
+          if(result){
+            switch(input.value){
+              case "attack":
+              case "Attack":
+
+              if(player.name === "Mage" || player.name === "Shaman"){
+                if(player.mana <= 0){
+                  rightOutput.value = "You dont have any  mana!";
+                } else {
+                  player.mana -= 20;
+                  let playerDmg = player.combat();
+                  let enemyDmg = enemy.attack - player.defense;
+                  if(enemyDmg < 0){
+                    enemyDmg = 0;
+                  }
+                  player.health -= enemyDmg;
+                  enemy.health -= playerDmg;
+                  choices();
+                }
+              } else {
+                let playerDmg = player.combat();
+                let enemyDmg = enemy.attack - player.defense;
+                if(enemyDmg < 0){
+                  enemyDmg = 0;
+                }
+
+                player.health -= enemyDmg;
+                enemy.health -= playerDmg;
+                choices();
+              }
+
+
+                break;
+              case "health":
+              case "Health":
+                if(player.potionCount >= 1){
+                  let input = document.getElementById("inputTextArea");
+                  input.value = "";
+                  if(player.health + player.potHealAmount > player.maxHealth){
+                    player.health = player.maxHealth;
+                    player.potionCount--;
+                    choices();
+                  } else {
+                    player.health += player.potHealAmount;
+                    player.potionCount--;
+                    choices();
+                  }
+                }
+                break;
+                case "mana":
+                case "Mana":
+                  if(player.manaPotionCount >= 1){
+                    let input = document.getElementById("inputTextArea");
+                    input.value = "";
+                    if(player.mana + player.manaPotionRecoverAmount > player.maxMana){
+                      player.mana = player.maxMana;
+                      player.manaPotionCount--;
+                      choices();
+                    } else {
+                      player.mana += player.manaPotionRecoverAmount;
+                      player.manaPotionCount--;
+                      choices();
+                    }
+                  }
+                  break;
+              case "run":
+              case "Run":
+                let input = document.getElementById("inputTextArea");
+                input.value = "";
+                town();
+              break;
+            }
+          }
+
+        }//end of choose()
+
+
+      } else if(player.health <= 0){
+        let output = document.getElementById("outputTextArea");
+        output.value = "\n\n\n\n\n\n\n\n\n\n\n\n\nYou run away, mortally wounded" +
+        "\nPress enter to continue";
+        inputSubmit.onclick = function(){
+          town();
+        }
+      } else if(enemy.health <= 0){
+        player.exp += exp;
+        levelCheck();
+        loot();
+        combat();
+      }//end of enemy health check if
+    }//end of choices()
+  } //end of leavetown()
+
+
+  leaveTown();
+}
+
+
+
+
+function levelCheck(){
+  let leftOutPut = document.getElementById("leftOutPut");
+  let input = document.getElementById("inputTextArea");
+  let body = document.getElementById("body");
+  removeName();
+  if(player.exp >= player.level * 1000){
+    player.maxHealth = parseInt(player.maxHealth * 1.2);
+    player.potHealAmount = parseInt(player.potHealAmount * 1.2);
+    player.health = parseInt(player.maxHealth);
+    levelUpScene();
+    player.level++;
+    leftOutPut.value = "-----------------------------" +
+    "\n-----------------------------" +
+    "\n-----------------------------" +
+    "\n-----------------------------" +
+    "\n-----------------------------" +
+    "\n-----------------------------" +
+    "\n----You are now level " + player.level + "!-----"+
+    "\n-----------------------------" +
+    "\n-----------------------------" +
+    "\n-----------------------------";
+
+  }
+}
+
+
+function removeName(){
+  let leftOutPut = document.getElementById("leftOutPut");
+  let rightOutput = document.getElementById("rightOutput");
+  let outputTextArea = document.getElementById("outputTextArea");
+  leftOutPut.style.animationName = "";
+  rightOutput.style.animationName = "";
+  outputTextArea.style.animationName = "";
+  leftOutPut.classList.remove('bounceInDown');
+  rightOutput.classList.remove('bounceInDown');
+  outputTextArea.classList.remove('bounceInDown');
+}
+
+
+
+function updateHealthBar(){
+    let frac = parseInt(((player.health/player.maxHealth) * 100)).toString() + "%";
+    let healthBar = document.getElementById("healthSpan");
+    healthBar.style.setProperty("--health-bar-height",frac);
+
+    let manaFrac = parseInt((player.mana/player.maxMana) * 100).toString() + "%";
+    let manaBar = document.getElementById("manaSpan");
+    manaSpan.style.setProperty("--mana-bar-height", manaFrac);
+}
